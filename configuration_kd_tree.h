0b0f361a62902baae1f3ff390b18d94042c48a73
Ido Kessler <Kessido@Gmail.com>
2018-06-09 11:14:53 +0300

    no kdtree - all connected small number of points
    the program now runs in loops searching for the first possible path, it uses 500 points so it could actually handle the load of n^2

--- configuration_kd_tree.h
+++ configuration_kd_tree.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "CGAL_defines.h"
+#include <CGAL/Range_segment_tree_traits.h>
+#include <CGAL/Range_tree_k.h>
 #include "node_configuration.h"
 #include <vector>
 #include <map>
@@ -13,51 +15,14 @@ private:
 	const Trap_pl &_trap_pl;
 	const FT _maximumDiameter;
 
-	KdTree _kdTree;
 	map<Point_2, shared_ptr<Configuration>> _point_to_configuration;
 	FT _connectionRadius;
 	Vector_2 _connectionRadiusVector;
 
-	void updateConnectionRadius() {
-		double s = _kdTree.size();
-		_connectionRadius = 10 * sqrt((1+log(s)) / s) * _maximumDiameter;
-		_connectionRadiusVector = { _connectionRadius , _connectionRadius };
-	}
-
 	shared_ptr<Configuration> to_configuration(const Point_2 &p) {
-		return shared_ptr<Configuration>(new Configuration(p));
+		return make_shared<Configuration>(p);
 	}
 
-	void add_to_kdTree(const Point_2 &conf) {
-		_kdTree.insert(conf);
-		updateConnectionRadius();
-	}
-
-	void add_to_kdTree(const vector<Point_2> &confs) {
-		_kdTree.insert(all(confs));
-		updateConnectionRadius();
-	}
-
-	vector<Point_2> getNeighbors(const Point_2 &p) {
-		Fuzzy_iso_box box(p - _connectionRadiusVector, p + _connectionRadiusVector);
-		vector<Point_2> res;
-		_kdTree.search(back_inserter(res), box);
-		return res;
-	}
-
-	vector<shared_ptr<Configuration>> getNeighborsConfiguration(const Point_2 &p) {
-		auto points = getNeighbors(p);
-		vector<shared_ptr<Configuration>> res;
-		res.reserve(points.size());
-		for (auto &point : points) {
-			res.pb(_point_to_configuration.find(point)->second);
-		}
-		return res;
-	}
-
-	vector<shared_ptr<Configuration>> getNeighborsConfiguration(const Configuration &p) {
-		return getNeighborsConfiguration(p.getPoint());
-	}
 	bool configurationIsValid(shared_ptr<Configuration> &conf) {
 		Arrangement_2::Face_handle face;
 		auto res = _trap_pl.locate(conf->getPoint());
@@ -69,16 +34,14 @@ private:
 		return true;
 	}
 
-	bool insert(shared_ptr<Configuration> &conf) {
+	void insert(shared_ptr<Configuration> &conf) {
 		if (!is_in_set(_point_to_configuration, conf->getPoint())
 			&& configurationIsValid(conf)) {
-			for (auto &conf_neigh : getNeighborsConfiguration(*conf)) {
-				Configuration::addNeighbor(conf, conf_neigh);
+			for (auto &conf_neigh : _point_to_configuration) {
+				Configuration::addNeighbor(conf, conf_neigh.second);
 			}
-			_point_to_configuration.insert(mp(conf->getPoint(), conf));
-			return true;
+			_point_to_configuration.emplace(conf->getPoint(), conf);
 		}
-		return false;
 	}
 
 
@@ -86,21 +49,15 @@ public:
 	ConfigurationKdTree(const Trap_pl &trap_pl, const FT maximumDiameter)
 		: _trap_pl(trap_pl), _maximumDiameter(maximumDiameter) {}
 
-	bool insert(const Point_2 &conf, bool shouldAddToKdTree = true) {
-		bool res = insert(to_configuration(conf));
-		if (res && shouldAddToKdTree)
-			add_to_kdTree(conf);
-		return res;
+	void insert(const Point_2 &conf) {
+		insert(to_configuration(conf));
 	}
 
 	void insert(const vector<Point_2> &confs) {
 		vector<Point_2> toAdd;
 		for (auto & conf : confs) {
-			if (insert(conf, false)) {
-				toAdd.pb(conf);
-			}
+			insert(conf);
 		}
-		add_to_kdTree(toAdd);
 	}
 
 	shared_ptr<Configuration> getConfiguration(const Point_2 &p) const {
--- path_finder.h
+++ path_finder.h
@@ -25,9 +25,7 @@ private:
 	};
 
 	typedef priority_queue<Pair_PairedConfiguration_AStarHuristic> my_priority_queue;
-	ConfigurationKdTree &_manager;
 	PairedConfiguration _start, _end;
-	bool pathFound = false;
 
 	bool DSU_checkForPathExistence(Arrangement_2& arr, const Trap_pl &trap_pl) {
 		set<PairedConfiguration> startSet, endSet;
@@ -57,48 +55,42 @@ private:
 
 
 public:
-	PathFinder(ConfigurationKdTree &manager, const shared_ptr<Configuration> &start1, const shared_ptr<Configuration> &end1,
+	PathFinder(const shared_ptr<Configuration> &start1, const shared_ptr<Configuration> &end1,
 		const shared_ptr<Configuration> &start2, const shared_ptr<Configuration> &end2) :
-		_manager(manager), _start(start1, start2), _end(end1, end2) {}
+		_start(start1, start2), _end(end1, end2) {}
 
 	vector<pair<Point_2, Point_2>> findPath(Arrangement_2& arr, const Trap_pl &trap_pl) {
 		if (_start == _end) return { (pair<Point_2, Point_2>) _start,(pair<Point_2, Point_2>) _end };
-		if (!pathFound) {
-			if (DSU_checkForPathExistence(arr, trap_pl)) {
-				pathFound = true;
-			}
-			else {
-				return {};
-			}
-		}
-		DistancesMap<PairedConfiguration> distances;
-		map<PairedConfiguration, PairedConfiguration> comeFrom;
-		set<PairedConfiguration> wasHere;
-		my_priority_queue q;
-		distances.set(_start, 0);
-		q.emplace(_start, _start.distanceFrom(_end));
-		while (!q.empty()) {
-			auto curr = q.top().conf; q.pop();
-			if (!is_in_set(wasHere, curr)) {
-				wasHere.insert(curr);
-				if (curr == _end) {
-					vector<pair<Point_2, Point_2>> res;
-					res.pb((pair<Point_2, Point_2>)_end);
-					while (curr != _start) {
-						curr = comeFrom.at(curr);
-						res.pb((pair<Point_2, Point_2>)curr);
+		if (DSU_checkForPathExistence(arr, trap_pl)) {
+			DistancesMap<PairedConfiguration> distances;
+			map<PairedConfiguration, PairedConfiguration> comeFrom;
+			set<PairedConfiguration> wasHere;
+			my_priority_queue q;
+			distances.set(_start, 0);
+			q.emplace(_start, _start.distanceFrom(_end));
+			while (!q.empty()) {
+				auto curr = q.top().conf; q.pop();
+				if (!is_in_set(wasHere, curr)) {
+					wasHere.insert(curr);
+					if (curr == _end) {
+						vector<pair<Point_2, Point_2>> res;
+						res.pb((pair<Point_2, Point_2>)_end);
+						while (curr != _start) {
+							curr = comeFrom.at(curr);
+							res.pb((pair<Point_2, Point_2>)curr);
+						}
+						reverse(all(res));
+						return res;
 					}
-					reverse(all(res));
-					return res;
-				}
-				for (auto &edge : curr.getNeighbors()) {
-					auto new_dist = distances.get(curr) + curr.distanceFrom(edge.getTarget());
-					auto old_dist = distances.get(edge.getTarget());
-					if (new_dist < old_dist && edge.verify(arr, trap_pl)) {
-						distances.set(edge.getTarget(), new_dist);
-						comeFrom.emplace(edge.getTarget(), curr);
-						auto huristic_distance = new_dist + curr.distanceFrom(_end);
-						q.emplace(edge.getTarget(), huristic_distance);
+					for (auto &edge : curr.getNeighbors()) {
+						auto new_dist = distances.get(curr) + curr.distanceFrom(edge.getTarget());
+						auto old_dist = distances.get(edge.getTarget());
+						if (new_dist < old_dist && edge.verify(arr, trap_pl)) {
+							distances.set(edge.getTarget(), new_dist);
+							comeFrom.emplace(edge.getTarget(), curr);
+							auto huristic_distance = new_dist + curr.distanceFrom(_end);
+							q.emplace(edge.getTarget(), huristic_distance);
+						}
 					}
 				}
 			}
